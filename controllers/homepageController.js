dc9e1f056cfb3105d974fa9d38c4ab2cc0415626
dimassusetyo <dimassusetyo11@gmail.com>
2021-10-12 13:46:42 +0700

    product detail

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -15,9 +15,7 @@ module.exports ={
             if(errCountParcel){
                 res.status(400).send(errCountParcel)
             }
-            totalParcels = resCountParcel[0].totalItems
-            console.log(totalParcels)
-            
+            totalParcels = resCountParcel[0].totalItems 
 
             let getParcel = `select * from parcel limit ${db.escape(parcelPerPage)} offset ${db.escape((currentParcelPage-1)*parcelPerPage)}`
 
@@ -38,10 +36,40 @@ module.exports ={
     },
     
 
-    //getParcelbyProductCategory
+    //getParcelDetail
+    getParcelDetail : (req,res) => {
+        const parcelDetail = `select p.idparcel, p.parcel_name, p.parcel_price, p.parcel_desc, p.parcel_image, c.category_name, d.qty_parcel_category from parcel p
+        left join parcel_detail d
+        on p.idparcel = d.idparcel
+        left join product_category c
+        on d.idproduct_category = c.idproduct_category
+        where p.idparcel = ${req.params.idparcel}`
+        db.query(parcelDetail, (errParcelDetail, resParcelDetail) => {
+            if (errParcelDetail) {
+                res.status(400).send(errParcelDetail)
+            }
+            
+            res.status(200).send(resParcelDetail)
+        })
+    },
 
 
     //sortParcelbyName
+    sortParcel : (req,res) => {
+    
+        const sortParcel = `select * from parcel order by ${req.params.sort} ${req.params.method} `
+        console.log(sortParcel)
+
+        // db.query(sortParcel, (errSortParcel, resSortParcel)=> {
+        //     if (errSortParcel) {
+        //         res.status(400).send(errSortParcel)
+        //     }
+        //     res.status(200).send(resSortParcel)
+        // })
+
+
+    }
+    
     
     //sortParcelbyPrice
     
