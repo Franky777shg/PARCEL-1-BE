c0ffe75d2c47e99ec9141b8bddaedcee978f9f59
Andra <andraandaruu@gmail.com>
2021-10-19 19:54:11 +0700

    PAR-29: Admin can see each parcel profit/loss, Refactor Get Revenue

--- routers/adminRevenueRouter.js
+++ routers/adminRevenueRouter.js
@@ -1,9 +1,6 @@
 const router = require("express").Router()
 const { adminRevenueController } = require("../controllers")
 
-router.get("/", adminRevenueController.getAllRevenue)
-router.post("/date", adminRevenueController.getRevenueByDate)
-router.post("/date-range", adminRevenueController.getRevenueByDateRange)
-router.post("/month", adminRevenueController.getRevenueByMonth)
+router.post("/", adminRevenueController.getRevenue)
 
 module.exports = router
